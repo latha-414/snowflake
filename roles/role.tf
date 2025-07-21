e257cc587e3ab2458107fa7493f4bfc18f0465cf
latha-414 <lathapaladugu414@gmail.com>
2025-07-21 14:52:47 +0530

    added terraform snowflake in all file

--- roles/role.tf
+++ roles/role.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    snowflake = {
+      source = "snowflakedb/snowflake"
+    }
+  }
+}
+
 resource "snowflake_role" "demo_role" {
   name = "DEMO_ROLE"
 }
--- users/create_user.tf
+++ users/create_user.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    snowflake = {
+      source = "snowflakedb/snowflake"
+    }
+  }
+}
+
 resource "snowflake_user" "demo_user" {
   name         = "SYS_latha"
   password     = "latha123"
