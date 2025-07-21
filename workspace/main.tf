b18614dd557ad2ffc3d2d96a38e2c73aab381ba3
latha-414 <144301489+latha-414@users.noreply.github.com>
2025-07-21 16:19:38 +0530

    Update main.tf

--- workspace/main.tf
+++ workspace/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     snowflake = {
       source  = "Snowflake-Labs/snowflake"
-      version = ">= 0.74.3"  # Latest recommended stable version
+      version = "~> 0.73.0"
     }
   }
 }
