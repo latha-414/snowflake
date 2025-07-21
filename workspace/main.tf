dda8b48f3ebd9ea0de3997e03f5bbf91e4f7e9a3
latha-414 <144301489+latha-414@users.noreply.github.com>
2025-07-21 16:03:30 +0530

    Update main.tf

--- workspace/main.tf
+++ workspace/main.tf
@@ -7,4 +7,9 @@ terraform {
   }
 }
 
-provider "snowflake" {}
+provider "snowflake" {
+  account  = var.snowflake_account       # "heygkbz-lw92235"
+  username = var.snowflake_username
+  password = var.snowflake_password
+  role     = var.snowflake_role
+}
