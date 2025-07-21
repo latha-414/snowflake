c30714d228cc9709dfdcd46278e046d8683fb38a
latha-414 <lathapaladugu414@gmail.com>
2025-07-21 15:11:05 +0530

    renamed

--- policies/policy.tf
+++ policies/policy.tf
@@ -1,12 +1,12 @@
-# terraform {
-#   required_providers {
-#     snowflake = {
-#       source = "snowflakedb/snowflake"
-#     }
-#   }
-# }
+terraform {
+  required_providers {
+    snowflake = {
+      source = "snowflakedb/snowflake"
+    }
+  }
+}
 
-# # resource "snowflake_network_policy" "demo_policy" {
-# #   name            = "DEMO_POLICY"
-# #   allowed_ip_list = ["192.168.1.0/24"]
-# # }
+# resource "snowflake_network_policy" "demo_policy" {
+#   name            = "DEMO_POLICY"
+#   allowed_ip_list = ["192.168.1.0/24"]
+# }
