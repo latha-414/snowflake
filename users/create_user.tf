28a8bd51e465c58c37735f9de2a0ab0a3742fea3
latha-414 <144301489+latha-414@users.noreply.github.com>
2025-07-21 16:48:02 +0530

    Update create_user.tf

--- users/create_user.tf
+++ users/create_user.tf
@@ -1,11 +1,11 @@
-terraform {
-  required_providers {
-    snowflake = {
-      source  = "Snowflake-Labs/snowflake"
-      version = "~> 0.73.0"
-    }
-  }
-}
+#terraform {
+ # required_providers {
+  #  snowflake = {
+   #   source  = "Snowflake-Labs/snowflake"
+    #  version = "~> 0.73.0"
+    #}
+  #}
+#}
 
 resource "snowflake_user" "demo_user" {
   name         = "SYS_latha"
