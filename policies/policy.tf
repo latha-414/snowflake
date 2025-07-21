f2703154b97c7a0a5cc8e9e23b8aeabe1df3e50f
latha-414 <144301489+latha-414@users.noreply.github.com>
2025-07-21 16:47:17 +0530

    Update policy.tf

--- policies/policy.tf
+++ policies/policy.tf
@@ -1,11 +1,11 @@
-terraform {
-  required_providers {
-    snowflake = {
-      source  = "Snowflake-Labs/snowflake"
-      version = "~> 0.73.0"
-    }
-  }
-}
+#terraform {
+ # required_providers {
+  #  snowflake = {
+   #   source  = "Snowflake-Labs/snowflake"
+    #  version = "~> 0.73.0"
+    #}
+  #}
+#}
 
 
 # resource "snowflake_network_policy" "demo_policy" {
