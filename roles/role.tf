c3bb892362f8799bfd7e0138ec38d935bf8684be
latha-414 <144301489+latha-414@users.noreply.github.com>
2025-07-21 16:47:41 +0530

    Update role.tf

--- roles/role.tf
+++ roles/role.tf
@@ -1,11 +1,11 @@
-terraform {
-  required_providers {
-    snowflake = {
-      source  = "Snowflake-Labs/snowflake"
-      version = "~> 0.73.0"
-    }
-  }
-}
+#terraform {
+ # required_providers {
+  #  snowflake = {
+   #   source  = "Snowflake-Labs/snowflake"
+    #  version = "~> 0.73.0"
+    #}
+  #}
+#}
 
 resource "snowflake_role" "demo_role" {
   name = "DEMO_ROLE"
