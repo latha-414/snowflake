0fc17a13844f704aa80d6dfd9d585f5cedf3f310
latha-414 <144301489+latha-414@users.noreply.github.com>
2025-07-21 15:16:11 +0530

    Update policy.tf

--- policies/policy.tf
+++ policies/policy.tf
@@ -1,7 +1,8 @@
 terraform {
   required_providers {
     snowflake = {
-      source = "snowflakedb/snowflake"
+      source  = "snowflakedb/snowflake"
+      version = ">= 0.74.3"  # Latest recommended stable version
     }
   }
 }
