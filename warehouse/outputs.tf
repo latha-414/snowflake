e8c82bf1905eab6a13f679d2895e5d6b4f51dc38
latha-414 <144301489+latha-414@users.noreply.github.com>
2025-07-22 15:39:57 +0530

    Update outputs.tf

--- warehouse/outputs.tf
+++ warehouse/outputs.tf
@@ -1,11 +1,14 @@
+output "warehouse_names" {
+  description = "List of all warehouse names created"
+  value       = module.warehouse.warehouse_names
+}
+
 output "user_names" {
-  value = module.users.user_names
+  description = "List of all user names created"
+  value       = module.users.user_names
 }
 
 output "role_names" {
-  value = module.roles.role_names
-}
-
-output "warehouse_names" {
-  value = module.warehouse.warehouse_names
+  description = "List of all role names created"
+  value       = module.roles.role_names
 }
