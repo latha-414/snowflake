7a581bc6ecf5eca26c04d96fefc90b9817247e81
latha-414 <144301489+latha-414@users.noreply.github.com>
2025-07-22 15:23:05 +0530

    Update create_warehouse.tf

--- warehouse/create_warehouse.tf
+++ warehouse/create_warehouse.tf
@@ -7,25 +7,17 @@ terraform {
   }
 }
 
-resource "snowflake_warehouse" "test_snow" {
-  name                          = "TEST_SNOW"
-  warehouse_size                = "XSMALL"
+resource "snowflake_warehouse" "new" {
+  for_each = var.new_warehouses
+
+  name                          = each.key
+  comment                       = each.value
+  warehouse_size                = "XSMALL"  # hardcoded size
   auto_suspend                  = 60
   auto_resume                   = false
   initially_suspended           = true
-  comment                       = "Testing warehouse"
   max_cluster_count             = 2
   min_cluster_count             = 1
   scaling_policy                = "STANDARD"
   statement_timeout_in_seconds = 300
 }
-
-# Uncomment if needed
-# resource "snowflake_warehouse_grant" "test_wh_ownership" {
-#   warehouse_name     = snowflake_warehouse.test_wh.name
-#   privilege          = "OWNERSHIP"
-#   roles              = ["DBA"]
-#   with_grant_option  = true
-#
-#   depends_on = [snowflake_warehouse.test_wh]
-# }
