3803ccce8433419e24bea554181cfa295060986d
latha-414 <144301489+latha-414@users.noreply.github.com>
2025-07-22 11:06:53 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    snowflake = {
+      source  = "Snowflake-Labs/snowflake"
+      version = "~> 0.73.0"
+    }
+  }
+}
+
+provider "snowflake" {}
+
+module "users" {
+  source = "../users"
+  providers = {
+    snowflake = snowflake
+  }
+}
+
+module "roles" {
+  source = "../roles"
+  providers = {
+    snowflake = snowflake
+  }
+}
+
+module "policies" {
+  source = "../policies"
+  providers = {
+    snowflake = snowflake
+  }
+}
