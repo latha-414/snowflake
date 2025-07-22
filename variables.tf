b536d138ff57ae9da5f1756a214447e1a7ec4977
latha-414 <144301489+latha-414@users.noreply.github.com>
2025-07-22 15:19:14 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,17 @@
+# Warehouses
 variable "new_warehouses" {
   description = "Map of warehouse names and comments"
   type        = map(string)
+}
+
+# Users
+variable "user_list" {
+  description = "List of Snowflake users to be created"
+  type        = list(string)
+}
+
+# Roles
+variable "role_list" {
+  description = "List of Snowflake roles to be created"
+  type        = list(string)
 }
