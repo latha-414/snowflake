24b1b85df4d40663d7a8f1e00b57d1d2a1998145
latha-414 <144301489+latha-414@users.noreply.github.com>
2025-07-23 12:36:53 +0530

    Update variables.tf

--- roles/variables.tf
+++ roles/variables.tf
@@ -2,3 +2,8 @@ variable "role_list" {
   description = "List of roles passed into the roles module"
   type        = list(string)
 }
+
+variable "role_owners" {
+  description = "Map of roles and their new owner roles"
+  type        = map(string)
+}
