16a22f78f3514241595a64cc3602a361a69f0895
latha-414 <144301489+latha-414@users.noreply.github.com>
2025-07-23 11:14:21 +0530

    Update variables.tf

--- users/variables.tf
+++ users/variables.tf
@@ -1,4 +1,10 @@
-variable "user_credentials" {
+variable "usernames" {
+  description = "List of usernames"
+  type        = set(string)
+  # Not marked as sensitive since usernames are typically not sensitive
+}
+
+variable "user_passwords" {
   description = "Map of usernames to passwords"
   type        = map(string)
   sensitive   = true
