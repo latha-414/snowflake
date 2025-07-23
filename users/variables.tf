a5ce8e93ff29e5ae465e9a494c28c3b78e4cf2c4
latha-414 <144301489+latha-414@users.noreply.github.com>
2025-07-23 10:27:21 +0530

    Update variables.tf

--- users/variables.tf
+++ users/variables.tf
@@ -1,4 +1,5 @@
-variable "user_list" {
-  description = "List of users passed into the users module"
-  type        = list(string)
+variable "user_credentials" {
+  description = "Map of usernames to passwords"
+  type        = map(string)
+  sensitive   = true
 }
